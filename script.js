c6606bc4e492fe258bd57315aab342d17edb4890
01062000 <dasrohit162000@gmail.com>
2023-05-14 15:46:41 +0530

    Implementation done

---
+++ script.js
@@ -0,0 +1,55 @@
+/** @format */
+
+console.log("app running");
+
+var currentTimeElement = document.querySelector(".time");
+var selectedTime = document.querySelector("#selectedTime");
+var alarmsContainer = document.querySelector(".alarmsContainer");
+
+var setAlarmBtn = document.querySelector(".setAlarmBtn");
+var currentTime;
+var alarmDiv;
+setAlarmBtn.addEventListener("click", () => {
+  console.log("time :" + currentTime);
+  console.log("selected time :" + selectedTime.value);
+  console.log(new Date(selectedTime.value));
+  alarmDiv = document.createElement("div");
+  alarmDiv.classList.add("alarmDiv");
+  var alarmPara = document.createElement('p');
+  alarmPara.innerText = selectedTime.value;
+  var delAlarmBtn = document.createElement('button');
+  delAlarmBtn.innerHTML = "Delete";
+  console.log(alarmsContainer);
+  delAlarmBtn.classList.add("delAlarmBtn");
+  alarmDiv.append(alarmPara);
+  alarmDiv.append(delAlarmBtn);
+  alarmsContainer.append(alarmDiv);
+  var delAlarmBtn = document.querySelector(".delAlarmBtn");
+  delAlarmBtn.addEventListener('click', () => {
+    alarmsContainer.removeChild(alarmDiv);
+    clearInterval(intervalID);
+  })
+
+  setAlarmInterval();
+
+});
+
+function setAlarm(){
+    if(currentTime == `${selectedTime.value}`){
+        clearInterval(intervalID);
+        alarmsContainer.removeChild(alarmDiv);
+        alert("Alarm Ringing");
+    }
+}
+var intervalID;
+function setAlarmInterval(){
+    intervalID =  setInterval(setAlarm, 1000);
+}
+
+function getCurrentTime() {
+  currentTime = new Date();
+  currentTime = currentTime.toLocaleTimeString("it-IT");
+  currentTimeElement.innerHTML = currentTime;
+}
+
+setInterval(getCurrentTime, 1000);
